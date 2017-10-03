mcfedr/awspushbundle

commit 1059d93bf4c7c147435a1e5b911dd117540a93ea

Change to v3 sdk paginator and exceptions

src/Mcfedr/AwsPushBundle/Command/EnableAllCommand.php
@@ -61,11 +61,9 @@ class EnableAllCommand extends Command
      */
     private function enablePlatform($platform)
     {
-        foreach ($this->sns->getListEndpointsByPlatformApplicationIterator(
-            [
-                'PlatformApplicationArn' => $this->arns[$platform]
-            ]
-        ) as $endpoint) {
+        foreach ($this->sns->getPaginator('ListEndpointsByPlatformApplication', [
+            'PlatformApplicationArn' => $this->arns[$platform]
+        ]) as $endpoint) {
             if ($endpoint['Attributes']['Enabled'] == "false") {
                 try {
                     $this->sns->setEndpointAttributes(

src/Mcfedr/AwsPushBundle/Command/RemoveDisabledCommand.php
@@ -61,11 +61,9 @@ class RemoveDisabledCommand extends Command
      */
     private function removeFromPlatform($platform)
     {
-        foreach ($this->sns->getListEndpointsByPlatformApplicationIterator(
-            [
-                'PlatformApplicationArn' => $this->arns[$platform]
-            ]
-        ) as $endpoint) {
+        foreach ($this->sns->getPaginator('ListEndpointsByPlatformApplication', [
+            'PlatformApplicationArn' => $this->arns[$platform]
+        ]) as $endpoint) {
             if ($endpoint['Attributes']['Enabled'] == "false") {
                 try {
                     $this->sns->deleteEndpoint(

src/Mcfedr/AwsPushBundle/Command/SubscribeTopicsCommand.php
@@ -1,8 +1,7 @@
 <?php
 namespace Mcfedr\AwsPushBundle\Command;
 
-use Aws\Sns\Exception\SubscriptionLimitExceededException;
-use Aws\Sns\Exception\TopicLimitExceededException;
+use Aws\Sns\Exception\SnsException;
 use Aws\Sns\SnsClient;
 use Mcfedr\AwsPushBundle\Service\Topics;
 use Psr\Log\LoggerInterface;
@@ -77,14 +76,7 @@ class SubscribeTopicsCommand extends Command
             foreach ($this->arns as $platform => $arn) {
                 $this->subscribePlatform($platform, $input->getOption('topic'));
             }
-        } catch (SubscriptionLimitExceededException $e) {
-            $this->logger && $this->logger->error(
-                'Failed to subscription to topic',
-                [
-                    'exception' => $e
-                ]
-            );
-        } catch (TopicLimitExceededException $e) {
+        } catch (SnsException $e) {
             $this->logger && $this->logger->error(
                 'Failed to create topic',
                 [
@@ -96,11 +88,9 @@ class SubscribeTopicsCommand extends Command
 
     private function subscribePlatform($platform, $topic)
     {
-        foreach ($this->sns->getListEndpointsByPlatformApplicationIterator(
-            [
-                'PlatformApplicationArn' => $this->arns[$platform]
-            ]
-        ) as $endpoint) {
+        foreach ($this->sns->getPaginator('ListEndpointsByPlatformApplication', [
+            'PlatformApplicationArn' => $this->arns[$platform]
+        ]) as $endpoint) {
             $this->logger && $this->logger->info(
                 'Subscribing device to topic',
                 [

src/Mcfedr/AwsPushBundle/Service/Messages.php
@@ -136,11 +136,9 @@ class Messages
             return;
         }
 
-        foreach ($this->sns->getListEndpointsByPlatformApplicationIterator(
-            [
-                'PlatformApplicationArn' => $this->arns[$platform]
-            ]
-        ) as $endpoint) {
+        foreach ($this->sns->getPaginator('ListEndpointsByPlatformApplication', [
+            'PlatformApplicationArn' => $this->arns[$platform]
+        ]) as $endpoint) {
             if ($endpoint['Attributes']['Enabled'] == "true") {
                 try {
                     $this->send($message, $endpoint['EndpointArn']);

src/Mcfedr/AwsPushBundle/Service/Devices.php
@@ -2,7 +2,7 @@
 
 namespace Mcfedr\AwsPushBundle\Service;
 
-use Aws\Sns\Exception\InvalidParameterException;
+use Aws\Sns\Exception\SnsException;
 use Aws\Sns\SnsClient;
 use Mcfedr\AwsPushBundle\Exception\PlatformNotConfiguredException;
 
@@ -53,7 +53,7 @@ class Devices
                     ]
                 ]
             );
-        } catch (InvalidParameterException $e) {
+        } catch (SnsException $e) {
             preg_match('/Endpoint (.+?) already/', $e->getMessage(), $matches);
             if (isset($matches[1])) {
                 $this->sns->setEndpointAttributes(

commit 33f0f297badaffe4b1a55d7f774f3aea36bbd5dd

Fix a bug with sending broadcast messages
Fixes iterating over AWS pagination result.

The bug was introduced in 1059d93bf4c7c147435a1e5b911dd117540a93ea

src/Mcfedr/AwsPushBundle/Command/EnableAllCommand.php
@@ -63,26 +63,28 @@ class EnableAllCommand extends Command
     {
         foreach ($this->sns->getPaginator('ListEndpointsByPlatformApplication', [
             'PlatformApplicationArn' => $this->arns[$platform]
-        ]) as $endpoint) {
-            if ($endpoint['Attributes']['Enabled'] == 'false') {
-                try {
-                    $this->sns->setEndpointAttributes(
-                        [
-                            'EndpointArn' => $endpoint['EndpointArn'],
-                            'Attributes' => [
-                                'Enabled' => 'true'
+        ]) as $endpointsResult) {
+            foreach ($endpointsResult['Endpoints'] as $endpoint) {
+                if ($endpoint['Attributes']['Enabled'] == 'false') {
+                    try {
+                        $this->sns->setEndpointAttributes(
+                            [
+                                'EndpointArn' => $endpoint['EndpointArn'],
+                                'Attributes' => [
+                                    'Enabled' => 'true'
+                                ]
                             ]
-                        ]
-                    );
-                    $this->logger && $this->logger->info("Enabled {$endpoint['EndpointArn']}");
-                } catch (\Exception $e) {
-                    $this->logger && $this->logger->error(
-                        "Failed to push set attributes on {$endpoint['EndpointArn']}",
-                        [
-                            'exception' => $e,
-                            'endpoint' => $endpoint
-                        ]
-                    );
+                        );
+                        $this->logger && $this->logger->info("Enabled {$endpoint['EndpointArn']}");
+                    } catch (\Exception $e) {
+                        $this->logger && $this->logger->error(
+                            "Failed to push set attributes on {$endpoint['EndpointArn']}",
+                            [
+                                'exception' => $e,
+                                'endpoint' => $endpoint
+                            ]
+                        );
+                    }
                 }
             }
         }

src/Mcfedr/AwsPushBundle/Command/RemoveDisabledCommand.php
@@ -63,23 +63,25 @@ class RemoveDisabledCommand extends Command
     {
         foreach ($this->sns->getPaginator('ListEndpointsByPlatformApplication', [
             'PlatformApplicationArn' => $this->arns[$platform]
-        ]) as $endpoint) {
-            if ($endpoint['Attributes']['Enabled'] == 'false') {
-                try {
-                    $this->sns->deleteEndpoint(
-                        [
-                            'EndpointArn' => $endpoint['EndpointArn']
-                        ]
-                    );
-                    $this->logger && $this->logger->info("Removed {$endpoint['EndpointArn']}");
-                } catch (\Exception $e) {
-                    $this->logger && $this->logger->error(
-                        "Failed to remove endpoint {$endpoint['EndpointArn']}",
-                        [
-                            'exception' => $e,
-                            'endpoint' => $endpoint
-                        ]
-                    );
+        ]) as $endpointsResult) {
+            foreach ($endpointsResult['Endpoints'] as $endpoint) {
+                if ($endpoint['Attributes']['Enabled'] == 'false') {
+                    try {
+                        $this->sns->deleteEndpoint(
+                            [
+                                'EndpointArn' => $endpoint['EndpointArn']
+                            ]
+                        );
+                        $this->logger && $this->logger->info("Removed {$endpoint['EndpointArn']}");
+                    } catch (\Exception $e) {
+                        $this->logger && $this->logger->error(
+                            "Failed to remove endpoint {$endpoint['EndpointArn']}",
+                            [
+                                'exception' => $e,
+                                'endpoint' => $endpoint
+                            ]
+                        );
+                    }
                 }
             }
         }

src/Mcfedr/AwsPushBundle/Command/SubscribeTopicsCommand.php
@@ -79,25 +79,27 @@ class SubscribeTopicsCommand extends Command
     {
         foreach ($this->sns->getPaginator('ListEndpointsByPlatformApplication', [
             'PlatformApplicationArn' => $this->arns[$platform]
-        ]) as $endpoint) {
-            $this->logger && $this->logger->info('Subscribing device to topic', [
-                'device' => $endpoint['EndpointArn'],
-                'topic' => $topic,
-                'platform' => $platform
-            ]);
-            try {
-                $this->sns->subscribe([
-                    'TopicArn' => $topic,
-                    'Protocol' => 'application',
-                    'Endpoint' => $endpoint['EndpointArn']
-                ]);
-            } catch (SnsException $e) {
-                $this->logger && $this->logger->info('Error subscribing device to topic', [
+        ]) as $endpointsResult) {
+            foreach ($endpointsResult['Endpoints'] as $endpoint) {
+                $this->logger && $this->logger->info('Subscribing device to topic', [
                     'device' => $endpoint['EndpointArn'],
                     'topic' => $topic,
-                    'platform' => $platform,
-                    'exception' => $e
+                    'platform' => $platform
                 ]);
+                try {
+                    $this->sns->subscribe([
+                        'TopicArn' => $topic,
+                        'Protocol' => 'application',
+                        'Endpoint' => $endpoint['EndpointArn']
+                    ]);
+                } catch (SnsException $e) {
+                    $this->logger && $this->logger->info('Error subscribing device to topic', [
+                        'device' => $endpoint['EndpointArn'],
+                        'topic' => $topic,
+                        'platform' => $platform,
+                        'exception' => $e
+                    ]);
+                }
             }
         }
     }

src/Mcfedr/AwsPushBundle/Service/Messages.php
@@ -144,28 +144,30 @@ class Messages
 
         foreach ($this->sns->getPaginator('ListEndpointsByPlatformApplication', [
             'PlatformApplicationArn' => $this->arns[$platform]
-        ]) as $endpoint) {
-            if ($endpoint['Attributes']['Enabled'] == 'true') {
-                try {
-                    $this->send($message, $endpoint['EndpointArn']);
-                } catch (\Exception $e) {
-                    $this->logger && $this->logger->error(
-                        "Failed to push to {$endpoint['EndpointArn']}",
+        ]) as $endpointsResult) {
+            foreach ($endpointsResult['Endpoints'] as $endpoint) {
+                if ($endpoint['Attributes']['Enabled'] == 'true') {
+                    try {
+                        $this->send($message, $endpoint['EndpointArn']);
+                    } catch (\Exception $e) {
+                        $this->logger && $this->logger->error(
+                            "Failed to push to {$endpoint['EndpointArn']}",
+                            [
+                                'Message' => $message,
+                                'Exception' => $e,
+                                'Endpoint' => $endpoint
+                            ]
+                        );
+                    }
+                } else {
+                    $this->logger && $this->logger->info(
+                        "Disabled endpoint {$endpoint['EndpointArn']}",
                         [
                             'Message' => $message,
-                            'Exception' => $e,
                             'Endpoint' => $endpoint
                         ]
                     );
                 }
-            } else {
-                $this->logger && $this->logger->info(
-                    "Disabled endpoint {$endpoint['EndpointArn']}",
-                    [
-                        'Message' => $message,
-                        'Endpoint' => $endpoint
-                    ]
-                );
             }
         }
     }