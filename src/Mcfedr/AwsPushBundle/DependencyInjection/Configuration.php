1da24bc96f2df5ec0035feb79b0d2f9290e5dd6e
Fred Cox <mcfedr@gmail.com>
2019-02-27 10:11:00 +0200

    Remove deprecated calls

--- src/Mcfedr/AwsPushBundle/DependencyInjection/Configuration.php
+++ src/Mcfedr/AwsPushBundle/DependencyInjection/Configuration.php
@@ -19,8 +19,16 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('mcfedr_aws_push')
+        $treeBuilder = new TreeBuilder('mcfedr_aws_push');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('mcfedr_aws_push');
+        }
+
+        $rootNode
             ->children()
                 ->arrayNode('aws')
                     ->children()
